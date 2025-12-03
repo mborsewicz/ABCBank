c7e9458a086a572cf9d563bc894e48ddb6bfa245
brsk <borsewicz.michal@gmail.com>
2025-12-03 19:30:04 +0100

    Refaktoryzacja klasy ReadRepositoryAsync
    Zmieniono klasę `ReadRepositoryAsync` na generyczną `ReadRepositoryAsync<T, Tid>`, dodając parametry generyczne oraz ograniczenie dla typu `T`.
    Dodano zależność od `ApplicationDbContext` i zaimplementowano metody `GetAllAsync` oraz `GetByIdAsync` do obsługi operacji asynchronicznych na danych.
    Zaktualizowano przestrzenie nazw w celu obsługi nowych funkcjonalności.

--- Infrastructure/Repositories/ReadRepositoryAsync.cs
+++ Infrastructure/Repositories/ReadRepositoryAsync.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Application.Repositories;
+using Domain.Contracts;
+using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Infrastructure.Repositories
 {
-    internal class ReadRepositoryAsync
+    internal class ReadRepositoryAsync<T, Tid> : IReadRepositoryAsync<T, Tid> where T : BaseEntity<Tid>
     {
+        private readonly ApplicationDbContext _context;
+        public ReadRepositoryAsync(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Tid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
     }
 }
