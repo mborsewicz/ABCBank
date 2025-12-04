c2bade5d6c015ec493f46918bf35174b5006d7f2
brsk <borsewicz.michal@gmail.com>
2025-12-04 19:56:44 +0100

    Dodano implementację wzorca Unit of Work
    Wprowadzono klasę `UnitOfWork<TId>` w przestrzeni nazw
    `Infrastructure.Repositories`, która implementuje interfejs
    `IUnitOfWork<TId>`. Klasa zarządza repozytoriami (`ReadRepositoryFor<T>`
    i `WriteRepositoryFor<T>`) oraz zapewnia mechanizm zapisu zmian w bazie
    danych za pomocą metody `CommitAsync`. Repozytoria są dynamicznie
    tworzone i przechowywane w kolekcji `_repositories`.
    
    Dodano obsługę zwalniania zasobów poprzez implementację wzorca
    `IDisposable`. Uzupełniono wymagane przestrzenie nazw (`using`)
    dla poprawnej implementacji.

---
+++ Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,76 @@
+﻿using Application.Repositories;
+using Domain.Contracts;
+using Infrastructure.Contexts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repositories
+{
+    internal class UnitOfWork<TId> : IUnitOfWork<TId>
+    {
+        private readonly ApplicationDbContext _context;
+        private bool _disposed;
+        private Hashtable _repositories;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CommitAsync(CancellationToken cancellationToken)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public IReadRepositoryAsync<T, TId> ReadRepositoryFor<T>() where T : BaseEntity<TId>
+        {
+            if (_repositories == null)
+            {
+                _repositories = new Hashtable();
+            }
+            var type = typeof(T).Name;
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(ReadRepositoryAsync<,>);
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(T), typeof(TId)), _context);
+                _repositories.Add(type, repositoryInstance);
+
+            }
+            return (IReadRepositoryAsync<T, TId>)_repositories[type];
+        }
+
+        public IWriteRepositoryAsync<T, TId> WriteRepositoryFor<T>() where T : BaseEntity<TId>
+        {
+            if (_repositories == null)
+            {
+                _repositories = new Hashtable();
+            }
+            var type = typeof(T).Name;
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(WriteRepositoryAsync<,>);
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(T), typeof(TId)), _context);
+                _repositories.Add(type, repositoryInstance);
+
+            }
+            return (IWriteRepositoryAsync<T, TId>)_repositories[type];
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+    }
+}
