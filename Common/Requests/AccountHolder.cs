85386f8f7af16844c5cd544934c74793225669eb
brsk <borsewicz.michal@gmail.com>
2025-12-10 21:03:20 +0100

    Dodano folder Responses i rekordy dla AccountHolder
    W pliku `Common.csproj` dodano grupę `<ItemGroup>` z folderem `Responses\`, co sugeruje rozszerzenie projektu o nowe zasoby.
    
    W pliku `AccountHolder.cs` dodano:
    - Przestrzenie nazw: `using System`, `using System.Collections.Generic`, `using System.Text`.
    - Rekordy w `Common.Requests`:
      - `CreateAccountHolder`: dane do tworzenia posiadacza konta.
      - `UpdateAccountHolder`: dane do aktualizacji posiadacza konta.

---
+++ Common/Requests/AccountHolder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Requests
+{
+    internal record CreateAccountHolder(string FirstName, string LastName, DateTime DateOfBirth,
+        string ContactNumber, string Email );
+
+    internal record UpdateAccountHolder(int Id, string FirstName, string LastName, DateTime DateOfBirth,
+    string ContactNumber, string Email);
+}
