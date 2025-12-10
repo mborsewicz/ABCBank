6c5e8f166974f6e884335e4eb3faa4b076b6551c
brsk <borsewicz.michal@gmail.com>
2025-12-10 20:52:52 +0100

    Dodano klasę ResponseWrapper<T> do obsługi odpowiedzi
    Dodano nowy plik `ResponseWrapper.cs` zawierający generyczną klasę `ResponseWrapper<T>` w przestrzeni nazw `Common.Wrapper`. Klasa umożliwia opakowanie odpowiedzi z właściwościami `IsSuccessful`, `Messages` oraz `Data`. Dodano metody `Success` i `Failed` do obsługi odpowiedzi udanych i nieudanych.

---
+++ Common/Wrapper/ResponseWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Wrapper
+{
+    public class ResponseWrapper<T>
+    {
+        public bool IsSuccessful { get; set; }
+        public List<string> Messages { get; set; }
+        public T Data { get; set; }
+        public ResponseWrapper<T> Success(T data, string message = null)
+        {
+            IsSuccessful = true;
+            Messages = [message];
+            Data = data;
+
+            return this;
+        }
+
+        public ResponseWrapper<T> Failed(string message = null)
+        {
+            IsSuccessful = false;
+            Messages = [message];
+            return this;
+        }
+
+    }
+}
