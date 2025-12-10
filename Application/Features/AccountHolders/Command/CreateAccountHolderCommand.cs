e53df7dbb82de4454e07fd85730e9a8fb2552bf2
brsk <borsewicz.michal@gmail.com>
2025-12-10 22:06:00 +0100

    Dodanie CreateAccountHolderCommand i integracja MediatR/Mapster
    Dodano obsługę komendy CreateAccountHolderCommand wraz z handlerem umożliwiającym tworzenie właścicieli kont. Zintegrowano biblioteki MediatR oraz Mapster w projekcie Application. Zmieniono modyfikatory rekordów CreateAccountHolder i UpdateAccountHolder na public, umożliwiając ich użycie poza bieżącym projektem.

---
+++ Application/Features/AccountHolders/Command/CreateAccountHolderCommand.cs
@@ -0,0 +1,34 @@
+﻿using Common.Wrapper;
+using Common.Requests;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Application.Repositories;
+using Mapster;
+using Domain;
+
+namespace Application.Features.AccountHolders.Command
+{
+    public class CreateAccountHolderCommand : IRequest<ResponseWrapper<int>>
+    {
+        public CreateAccountHolder CreateAccountHolder { get; set; }
+    }
+
+    public class CreateAccountHolderCommandHandler : IRequestHandler<CreateAccountHolderCommand, ResponseWrapper<int>>
+    {
+        private readonly IUnitOfWork<int> _unitOfWork;
+        public CreateAccountHolderCommandHandler(IUnitOfWork<int> unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<ResponseWrapper<int>> Handle(CreateAccountHolderCommand request, CancellationToken cancellationToken)
+        {
+            var accountHolder = request.CreateAccountHolder.Adapt<AccountHolder>();
+            
+            await _unitOfWork.WriteRepositoryFor<AccountHolder>().AddAsync(accountHolder);
+            await _unitOfWork.CommitAsync(cancellationToken);
+            return new ResponseWrapper<int>().Success(accountHolder.Id, "Account holder created successfully.");
+        }
+    }
+}
--- Common/Requests/AccountHolder.cs
+++ Common/Requests/AccountHolder.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace Common.Requests
 {
-    internal record CreateAccountHolder(string FirstName, string LastName, DateTime DateOfBirth,
+    public record CreateAccountHolder(string FirstName, string LastName, DateTime DateOfBirth,
         string ContactNumber, string Email );
 
-    internal record UpdateAccountHolder(int Id, string FirstName, string LastName, DateTime DateOfBirth,
+    public record UpdateAccountHolder(int Id, string FirstName, string LastName, DateTime DateOfBirth,
     string ContactNumber, string Email);
 }
