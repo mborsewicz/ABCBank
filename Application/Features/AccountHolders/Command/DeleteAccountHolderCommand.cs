3e2c9bdd293641bac796bdcf2b006f590df7ced9
brsk <borsewicz.michal@gmail.com>
2026-01-21 19:20:25 +0100

    Dodaj komendę usuwania posiadacza konta i handler
    Dodano DeleteAccountHolderCommand oraz handlera, umożliwiając usuwanie posiadacza konta po Id. Handler sprawdza istnienie rekordu, usuwa go i zwraca odpowiedź w ResponseWrapper. Wprowadzono także wymagane usingi i zależność do IUnitOfWork.

---
+++ Application/Features/AccountHolders/Command/DeleteAccountHolderCommand.cs
@@ -0,0 +1,36 @@
+﻿using Application.Repositories;
+using Common.Wrapper;
+using Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Features.AccountHolders.Command
+{
+    public class DeleteAccountHolderCommand : IRequest<ResponseWrapper<int>>
+    {
+        
+        public int Id { get; set; }
+
+    }
+
+    public class DeleteAccountHolderCommandHandler : IRequestHandler<DeleteAccountHolderCommand, ResponseWrapper<int>>
+    {
+        private readonly IUnitOfWork<int> _unitOfWork;
+
+        public async Task<ResponseWrapper<int>> Handle(DeleteAccountHolderCommand request, CancellationToken cancellationToken)
+        {
+            var accountHolderInDb = await _unitOfWork.ReadRepositoryFor<AccountHolder>().GetByIdAsync(request.Id);
+
+            if (accountHolderInDb is not null)
+            {
+                await _unitOfWork.WriteRepositoryFor<AccountHolder>().DeleteAsync(accountHolderInDb);
+                await _unitOfWork.CommitAsync(cancellationToken);
+
+                return new ResponseWrapper<int>().Success(accountHolderInDb.Id, "Account holder deleted successfully.");
+            }
+            return new ResponseWrapper<int>().Failed("Account holder not found.");
+        }
+    }
+}
