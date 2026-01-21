b770cc7aece4fbdd12f5c19bbac6654a4c8b701f
brsk <borsewicz.michal@gmail.com>
2026-01-21 19:55:16 +0100

    Dodano CQRS GetAccountHolderById oraz AccountHolderResponse
    Dodano zapytanie CQRS (MediatR) do pobierania właściciela konta po ID wraz z handlerem korzystającym z IUnitOfWork. Dodano klasę AccountHolderResponse do zwracania danych właściciela konta. Usunięto wpisy <Folder> z plików .csproj.

---
+++ Application/Features/AccountHolders/Queries/GetAccountHolderByIdQuery.cs
@@ -0,0 +1,35 @@
+﻿using Application.Repositories;
+using Common.Responses;
+using Common.Wrapper;
+using Domain;
+using Mapster;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Features.AccountHolders.Queries
+{
+    public class GetAccountHolderByIdQuery : IRequest<ResponseWrapper<AccountHolderResponse>>
+    {
+        public int Id { get; set; }
+        public class GetAccountHolderByIdQueryHandler : IRequestHandler<GetAccountHolderByIdQuery, ResponseWrapper<AccountHolderResponse>>
+        {
+            private readonly IUnitOfWork<int> _unitOfWork;
+            public GetAccountHolderByIdQueryHandler(IUnitOfWork<int> unitOfWork)
+            {
+                _unitOfWork = unitOfWork;
+            }
+            public async Task<ResponseWrapper<AccountHolderResponse>> Handle(GetAccountHolderByIdQuery request, CancellationToken cancellationToken)
+            {
+               var accountHolderInDb = await _unitOfWork.ReadRepositoryFor<AccountHolder>().GetByIdAsync(request.Id);
+
+                if (accountHolderInDb is not null)
+                {
+                     return new ResponseWrapper<AccountHolderResponse>().Success(accountHolderInDb.Adapt<AccountHolderResponse>());
+                }
+                return new ResponseWrapper<AccountHolderResponse>().Failed("No account holder found with the given id");
+            }
+        }
+    }
+}
---
+++ Common/Responses/AccountHolderResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Responses
+{
+    public class AccountHolderResponse
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
