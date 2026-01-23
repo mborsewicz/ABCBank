4277252bbdd6ba2fc62a979c03ce07d688eb9aec
brsk <borsewicz.michal@gmail.com>
2026-01-23 18:20:03 +0100

    Dodano endpointy CRUD dla AccountHolder i obsługę braku danych
    Dodano endpointy PUT, DELETE, GET (po id i wszystkie) w AccountHoldersController. Wprowadzono obsługę przypadku braku właścicieli kont w GetAccountHoldersQuery. Dodano brakujący using do zapytań w kontrolerze.

--- WebApi/Controllers/AccountHoldersController.cs
+++ WebApi/Controllers/AccountHoldersController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Features.AccountHolders.Command;
+using Application.Features.AccountHolders.Queries;
 using Common.Requests;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +22,51 @@ namespace WebApi.Controllers
             }
             return BadRequest(response);
 
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> UpdateAccountHolderAsync([FromBody] UpdateAccountHolder updateAccountHolder)
+        {
+            var response = await Sender.Send(new UpdateAccountHolderCommand { UpdateAccountHolder = updateAccountHolder });
+            if (response.IsSuccessful)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAccountHolderAsync(int id)
+        {
+            var response = await Sender.Send(new DeleteAccountHolderCommand { Id = id });
+            if (response.IsSuccessful)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAccountHolderByIdAsync(int id)
+        {
+            var response = await Sender.Send(new GetAccountHolderByIdQuery { Id = id });
+            if (response.IsSuccessful)
+            {
+                return Ok(response);
+            }
+            return NotFound(response);
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllAccountHoldersAsync()
+        {
+            var response = await Sender.Send(new GetAccountHoldersQuery());
+            if (response.IsSuccessful)
+            {
+                return Ok(response);
+            }
+            return NotFound(response);
         }
     }
 }
