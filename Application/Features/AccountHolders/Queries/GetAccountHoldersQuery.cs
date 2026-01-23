3d7d7e1280b3aee37d4070fc9b897d7b265a74b7
brsk <borsewicz.michal@gmail.com>
2026-01-23 17:48:31 +0100

    Dodaj zapytanie GetAccountHoldersQuery i handler
    Dodano plik GetAccountHoldersQuery.cs z zapytaniem oraz handlerem umożliwiającym pobieranie listy posiadaczy kont z bazy danych przy użyciu wzorca UnitOfWork. Wynik jest mapowany na AccountHolderResponse i zwracany w opakowanej odpowiedzi ResponseWrapper. Dodano wymagane usingi.

---
+++ Application/Features/AccountHolders/Queries/GetAccountHoldersQuery.cs
@@ -0,0 +1,31 @@
+﻿using Application.Repositories;
+using Common.Responses;
+using Common.Wrapper;
+using Domain;
+using Mapster;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Features.AccountHolders.Queries
+{
+    public class GetAccountHoldersQuery : IRequest<ResponseWrapper<List<AccountHolderResponse>>>
+    {
+    }
+
+    public class GetAccountHoldersQueryHandler : IRequestHandler<GetAccountHoldersQuery, ResponseWrapper<List<AccountHolderResponse>>>
+    {
+        private readonly IUnitOfWork<int> _unitOfWork;
+        public GetAccountHoldersQueryHandler(IUnitOfWork<int> unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<ResponseWrapper<List<AccountHolderResponse>>> Handle(GetAccountHoldersQuery request, CancellationToken cancellationToken)
+        {
+            var accountHoldersInDb = await _unitOfWork.ReadRepositoryFor<AccountHolder>().GetAllAsync();
+
+            return new ResponseWrapper<List<AccountHolderResponse>>().Success(accountHoldersInDb.Adapt<List<AccountHolderResponse>>());
+        }
+    }
+}
