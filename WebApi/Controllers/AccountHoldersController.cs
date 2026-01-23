0b584315e6dc970065afe3c99cf84fb2134b2c05
brsk <borsewicz.michal@gmail.com>
2026-01-23 18:04:44 +0100

    Dodaj kontrolery AccountHolders i bazowy BaseApiController
    Dodano AccountHoldersController z endpointem POST do dodawania właścicieli kont oraz BaseApiController z obsługą MediatR (CQRS). Ułatwia to implementację kolejnych kontrolerów i centralizuje obsługę żądań poprzez MediatR.

---
+++ WebApi/Controllers/AccountHoldersController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.AccountHolders.Command;
+using Common.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountHoldersController : BaseApiController
+    {
+        [HttpPost("add")]
+        public async Task<IActionResult> AddAccountHolderAsync([FromBody] CreateAccountHolder createAccountHolder)
+        {
+            var response = await Sender.Send(new CreateAccountHolderCommand { CreateAccountHolder = createAccountHolder });
+
+            if (response.IsSuccessful)
+            {
+                return Ok(response);
+
+            }
+            return BadRequest(response);
+
+        }
+    }
+}
---
+++ WebApi/Controllers/BaseApiController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        private ISender _sender = null;
+        public ISender Sender => _sender ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
