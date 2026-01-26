f1625691d3de27f5aa51a5b2222c8445890cdb96
brsk <borsewicz.michal@gmail.com>
2026-01-26 18:14:03 +0100

    Dodaj AccountController z obsługą tworzenia kont
    Dodano kontroler AccountController w WebApi z endpointem POST "api/account/add" umożliwiającym tworzenie nowych kont. Zaimplementowano metodę AddAccountAsync, która przyjmuje CreateAccountRequest, wysyła polecenie CreateAccountCommand i zwraca odpowiedni status HTTP w zależności od wyniku operacji. Dodano wymagane usingi.

---
+++ WebApi/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Accounts.Commands;
+using Common.Requests;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : BaseApiController
+    {
+        [HttpPost("add")]
+        public async Task<IActionResult> AddAccountAsync([FromBody] CreateAccountRequest createAccountRequest)
+        {
+            var response = await Sender.Send(new CreateAccountCommand { CreateAccount = createAccountRequest });
+            if (response.IsSuccessful)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response);
+        }
+    }
+}
